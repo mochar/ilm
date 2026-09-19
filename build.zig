add661c36256c9faa172f12c74c910c9d9e3ae73
mochar <mocharrout@gmail.com>
2026-09-19 20:16:05 +0200

    slopped but better graphviz build

--- build.zig
+++ build.zig
@@ -146,26 +146,413 @@ fn buildGraphviz(
     c_mod: *Build.Module,
     c_bindings: *Build.Step.TranslateC,
 ) BuildPart {
-    const is_release = optimize != .Debug;
-
     const cmake_cfg = b.addSystemCommand(&.{
         "cmake", "-B", "vendor/graphviz/build", "-S", "vendor/graphviz",
     });
-    if (is_release) {
+    if (optimize != .Debug) {
         cmake_cfg.addArgs(&.{"-DCMAKE_BUILD_TYPE=Release"});
     }
 
-    const cmake_build = b.addSystemCommand(&.{ "cmake", "--build", "vendor/graphviz/build", "--parallel" });
-
-    // Only configure if CMakeCache.txt doesn't exist.
-    // cmake --build will automatically reconfigure if CMakeLists.txt changes.
-    std.Io.Dir.accessAbsolute(b.graph.io, b.pathFromRoot("vendor/graphviz/build/CMakeCache.txt"), .{}) catch {
-        cmake_build.step.dependOn(&cmake_cfg.step);
+    // Only configure if generated grammar.c doesn't exist yet.
+    var cmake_needed = false;
+    std.Io.Dir.accessAbsolute(b.graph.io, b.pathFromRoot("vendor/graphviz/build/lib/cgraph/grammar.c"), .{}) catch {
+        cmake_needed = true;
     };
 
+    // Header files setup for <graphviz/...> includes
+    const inc_files = b.addWriteFiles();
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/cgraph/cgraph.h"), "graphviz/cgraph.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/gvc/gvc.h"), "graphviz/gvc.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/cdt/cdt.h"), "graphviz/cdt.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/gvc/gvcext.h"), "graphviz/gvcext.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/gvc/gvcjob.h"), "graphviz/gvcjob.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/gvc/gvcommon.h"), "graphviz/gvcommon.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/gvc/gvconfig.h"), "graphviz/gvconfig.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/gvc/gvplugin.h"), "graphviz/gvplugin.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/gvc/gvplugin_device.h"), "graphviz/gvplugin_device.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/gvc/gvplugin_layout.h"), "graphviz/gvplugin_layout.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/gvc/gvplugin_loadimage.h"), "graphviz/gvplugin_loadimage.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/gvc/gvplugin_render.h"), "graphviz/gvplugin_render.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/gvc/gvplugin_textlayout.h"), "graphviz/gvplugin_textlayout.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/pathplan/pathgeom.h"), "graphviz/pathgeom.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/pathplan/pathplan.h"), "graphviz/pathplan.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/pack/pack.h"), "graphviz/pack.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/xdot/xdot.h"), "graphviz/xdot.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/common/arith.h"), "graphviz/arith.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/common/color.h"), "graphviz/color.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/common/geom.h"), "graphviz/geom.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/common/textspan.h"), "graphviz/textspan.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/common/types.h"), "graphviz/types.h");
+    _ = inc_files.addCopyFile(b.path("vendor/graphviz/lib/common/usershape.h"), "graphviz/usershape.h");
+
+    // Config header
+    const config_h_source =
+        \\#pragma once
+        \\
+        \\#if !defined(_WIN32)
+        \\#define HAVE_SYS_IOCTL_H
+        \\#define HAVE_SYS_MMAN_H
+        \\#define HAVE_SYS_SELECT_H
+        \\#define HAVE_SYS_TIME_H
+        \\#define HAVE_GETOPT_H
+        \\#define HAVE_DRAND48
+        \\#define HAVE_MEMRCHR
+        \\#define HAVE_SETENV
+        \\#define HAVE_SRAND48
+        \\#define HAVE_STRCASESTR
+        \\#endif
+        \\
+        \\#define BROWSER "xdg-open"
+        \\#define DEFAULT_DPI 96
+        \\#define GVPLUGIN_CONFIG_FILE "config8"
+        \\#define PACKAGE_VERSION "16.1.1"
+    ;
+    _ = inc_files.add("config.h", config_h_source);
+
+    c_bindings.addIncludePath(inc_files.getDirectory());
+    c_bindings.addIncludePath(b.path("vendor/graphviz/lib"));
     c_bindings.addIncludePath(b.path("vendor/graphviz/lib/cgraph"));
     c_bindings.addIncludePath(b.path("vendor/graphviz/lib/gvc"));
-    c_bindings.addIncludePath(b.path("vendor/graphviz/build")); // For generated headers
+    c_bindings.addIncludePath(b.path("vendor/graphviz/lib/cdt"));
+    c_bindings.addIncludePath(b.path("vendor/graphviz/build"));
+    c_bindings.addIncludePath(b.path("vendor/graphviz/build/lib/common"));
+
+    // Builtins and plugin registration with hardcoded layout algorithms: dot, dotx, neato
+    const builtins_source =
+        \\#include "config.h"
+        \\#include <gvc/gvc.h>
+        \\#include <gvc/gvplugin.h>
+        \\#include <gvc/gvplugin_layout.h>
+        \\#include <gvc/gvplugin_render.h>
+        \\#include <gvc/gvplugin_device.h>
+        \\#include <gvc/gvplugin_loadimage.h>
+        \\#include <gvc/gvcext.h>
+        \\
+        \\// dot layout engine
+        \\extern void dot_layout(graph_t *g);
+        \\extern void dot_cleanup(graph_t *g);
+        \\
+        \\static gvlayout_engine_t dotgen_engine = {
+        \\    dot_layout,
+        \\    dot_cleanup,
+        \\};
+        \\
+        \\static gvlayout_features_t dotgen_features = {
+        \\    LAYOUT_USES_RANKDIR,
+        \\};
+        \\
+        \\static gvplugin_installed_t gvlayout_dot_types[] = {
+        \\    {0, "dot", 0, &dotgen_engine, &dotgen_features},
+        \\    {0, "dotx", 0, &dotgen_engine, &dotgen_features},
+        \\    {0, NULL, 0, NULL, NULL}
+        \\};
+        \\
+        \\static gvplugin_api_t dot_apis[] = {
+        \\    {API_layout, gvlayout_dot_types},
+        \\    {(api_t)0, 0},
+        \\};
+        \\
+        \\gvplugin_library_t gvplugin_dot_layout_LTX_library = { "dot_layout", dot_apis };
+        \\
+        \\// neato layout engine
+        \\extern void neato_layout(graph_t *g);
+        \\extern void neato_cleanup(graph_t *g);
+        \\
+        \\static gvlayout_engine_t neatogen_engine = {
+        \\    neato_layout,
+        \\    neato_cleanup,
+        \\};
+        \\
+        \\static gvlayout_features_t neatogen_features = {
+        \\    0,
+        \\};
+        \\
+        \\static void nop1_layout(graph_t *g) {
+        \\    extern int Nop;
+        \\    Nop = 1;
+        \\    neato_layout(g);
+        \\    Nop = 0;
+        \\}
+        \\
+        \\static void nop2_layout(graph_t *g) {
+        \\    extern int Nop;
+        \\    Nop = 2;
+        \\    neato_layout(g);
+        \\    Nop = 0;
+        \\}
+        \\
+        \\static gvlayout_engine_t nop1gen_engine = {
+        \\    nop1_layout,
+        \\    neato_cleanup,
+        \\};
+        \\
+        \\static gvlayout_engine_t nop2gen_engine = {
+        \\    nop2_layout,
+        \\    neato_cleanup,
+        \\};
+        \\
+        \\static gvplugin_installed_t gvlayout_neato_types[] = {
+        \\    {0, "neato", 0, &neatogen_engine, &neatogen_features},
+        \\    {0, "nop", 0, &nop1gen_engine, &neatogen_features},
+        \\    {0, "nop1", 0, &nop1gen_engine, &neatogen_features},
+        \\    {0, "nop2", 0, &nop2gen_engine, &neatogen_features},
+        \\    {0, NULL, 0, NULL, NULL}
+        \\};
+        \\
+        \\static gvplugin_api_t neato_apis[] = {
+        \\    {API_layout, gvlayout_neato_types},
+        \\    {(api_t)0, 0},
+        \\};
+        \\
+        \\gvplugin_library_t gvplugin_neato_layout_LTX_library = { "neato_layout", neato_apis };
+        \\
+        \\// core plugin declaration (renderers, devices)
+        \\extern gvplugin_library_t gvplugin_core_LTX_library;
+        \\
+        \\// Preloaded symbol table containing our built-in plugins
+        \\lt_symlist_t lt_preloaded_symbols[] = {
+        \\    { "gvplugin_dot_layout_LTX_library", &gvplugin_dot_layout_LTX_library },
+        \\    { "gvplugin_neato_layout_LTX_library", &gvplugin_neato_layout_LTX_library },
+        \\    { "gvplugin_core_LTX_library", &gvplugin_core_LTX_library },
+        \\    { 0, 0 }
+        \\};
+        \\
+        \\#undef gvContext
+        \\GVC_t *gvContext(void) {
+        \\    return gvContextPlugins(lt_preloaded_symbols, 0);
+        \\}
+    ;
+
+    const builtins_file = inc_files.add("builtins.c", builtins_source);
+
+    const lib_mod = b.createModule(.{
+        .link_libc = true,
+        .target = target,
+        .optimize = optimize,
+    });
+
+    lib_mod.addIncludePath(inc_files.getDirectory());
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib/cdt"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib/cgraph"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib/common"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib/gvc"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib/pathplan"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib/pack"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib/xdot"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib/neatogen"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib/dotgen"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib/sparse"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib/rbtree"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib/label"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/lib/util"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/plugin/core"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/build"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/build/lib/cgraph"));
+    lib_mod.addIncludePath(b.path("vendor/graphviz/build/lib/common"));
+
+    const c_flags = &[_][]const u8{
+        "-std=gnu11",
+        "-DEXPORT_CDT",
+        "-DEXPORT_CGRAPH",
+        "-DEXPORT_CGHDR",
+        "-DPATHPLAN_EXPORTS",
+        "-DEXPORT_XDOT",
+        "-DGVC_EXPORTS",
+        "-DNEATOGEN_EXPORTS=1",
+        "-DGVLIBDIR=\"\"",
+        "-DgvContext=orig_gvContext",
+        "-Wno-unused-parameter",
+        "-Wno-sign-compare",
+        "-Wno-unused-function",
+        "-Wno-implicit-fallthrough",
+        "-Wno-deprecated-declarations",
+        "-Wno-unused-but-set-variable",
+        "-Wno-strict-prototypes",
+        "-Wno-incompatible-pointer-types",
+        "-Wno-return-type",
+    };
+
+    // cdt
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/lib/cdt"),
+        .files = &.{
+            "dtclose.c", "dtdisc.c",   "dtextract.c", "dtflatten.c", "dthash.c",
+            "dtmethod.c", "dtopen.c",   "dtrenew.c",   "dtrestore.c", "dtsize.c",
+            "dtstat.c",   "dtstrhash.c", "dttree.c",   "dtview.c",    "dtwalk.c",
+        },
+        .flags = c_flags,
+    });
+
+    // cgraph
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/lib/cgraph"),
+        .files = &.{
+            "acyclic.c", "agerror.c", "apply.c", "attr.c", "edge.c",
+            "graph.c", "id.c", "imap.c", "ingraphs.c", "io.c",
+            "node.c", "node_induce.c", "obj.c", "rec.c", "refstr.c",
+            "subg.c", "tred.c", "unflatten.c", "utils.c", "write.c",
+        },
+        .flags = c_flags,
+    });
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/build/lib/cgraph"),
+        .files = &.{ "grammar.c", "scan.c" },
+        .flags = c_flags,
+    });
+
+    // util
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/lib/util"),
+        .files = &.{
+            "arena.c", "base64.c", "gv_find_me.c", "gv_fopen.c",
+            "list.c", "random.c", "xml.c",
+        },
+        .flags = c_flags,
+    });
+
+    // pathplan
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/lib/pathplan"),
+        .files = &.{
+            "cvt.c", "inpoly.c", "route.c", "shortest.c",
+            "shortestpth.c", "solvers.c", "triang.c", "util.c", "visibility.c",
+        },
+        .flags = c_flags,
+    });
+
+    // pack
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/lib/pack"),
+        .files = &.{ "ccomps.c", "pack.c" },
+        .flags = c_flags,
+    });
+
+    // xdot
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/lib/xdot"),
+        .files = &.{ "xdot.c" },
+        .flags = c_flags,
+    });
+
+    // label
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/lib/label"),
+        .files = &.{
+            "index.c", "node.c", "rectangle.c", "split.q.c", "xlabels.c",
+        },
+        .flags = c_flags,
+    });
+
+    // common
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/lib/common"),
+        .files = &.{
+            "args.c", "arrows.c", "colxlate.c", "ellipse.c", "emit.c",
+            "geom.c", "globals.c", "htmllex.c", "htmltable.c", "input.c",
+            "labels.c", "ns.c", "output.c", "pointset.c", "postproc.c",
+            "psusershape.c", "routespl.c", "shapes.c", "splines.c", "taper.c",
+            "textspan.c", "textspan_lut.c", "timing.c", "utils.c",
+        },
+        .flags = c_flags,
+    });
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/build/lib/common"),
+        .files = &.{ "htmlparse.c" },
+        .flags = c_flags,
+    });
+
+    // gvc
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/lib/gvc"),
+        .files = &.{
+            "gvc.c", "gvconfig.c", "gvcontext.c", "gvdevice.c", "gvevent.c",
+            "gvjobs.c", "gvlayout.c", "gvloadimage.c", "gvplugin.c", "gvrender.c",
+            "gvtextlayout.c", "gvtool_tred.c", "gvusershape.c",
+        },
+        .flags = c_flags,
+    });
+
+    // dot layout (dotgen)
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/lib/dotgen"),
+        .files = &.{
+            "acyclic.c", "aspect.c", "class1.c", "class2.c", "cluster.c",
+            "compound.c", "conc.c", "decomp.c", "dotinit.c", "dotsplines.c",
+            "fastgr.c", "flat.c", "mincross.c", "position.c", "rank.c", "sameport.c",
+        },
+        .flags = c_flags,
+    });
+
+    // neato layout (neatogen, sparse, rbtree)
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/lib/neatogen"),
+        .files = &.{
+            "adjust.c", "bfs.c", "call_tri.c", "circuit.c", "closest.c",
+            "compute_hierarchy.c", "conjgrad.c", "constrained_majorization.c", "constraint.c", "delaunay.c",
+            "dijkstra.c", "edges.c", "embed_graph.c", "geometry.c", "heap.c",
+            "hedges.c", "info.c", "kkutils.c", "legal.c", "lu.c",
+            "matinv.c", "matrix_ops.c", "multispline.c", "neatoinit.c", "neatosplines.c",
+            "opt_arrangement.c", "overlap.c", "pca.c", "poly.c", "quad_prog_solve.c",
+            "randomkit.c", "sgd.c", "site.c", "smart_ini_x.c", "solve.c",
+            "stress.c", "stuff.c", "voronoi.c",
+        },
+        .flags = c_flags,
+    });
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/lib/sparse"),
+        .files = &.{
+            "clustering.c", "color_palette.c", "colorutil.c", "DotIO.c",
+            "general.c", "mq.c", "QuadTree.c", "SparseMatrix.c",
+        },
+        .flags = c_flags,
+    });
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/lib/rbtree"),
+        .files = &.{ "red_black_tree.c" },
+        .flags = c_flags,
+    });
+
+    // core plugins (renderers: dot, xdot, svg, json, plain, etc.)
+    lib_mod.addCSourceFiles(.{
+        .root = b.path("vendor/graphviz/plugin/core"),
+        .files = &.{
+            "gvplugin_core.c", "gvrender_core_dot.c", "gvrender_core_fig.c", "gvrender_core_json.c",
+            "gvrender_core_map.c", "gvrender_core_pic.c", "gvrender_core_pov.c", "gvrender_core_ps.c",
+            "gvrender_core_svg.c", "gvrender_core_tk.c", "gvloadimage_core.c",
+        },
+        .flags = c_flags,
+    });
+
+    // Builtins registration
+    lib_mod.addCSourceFile(.{
+        .file = builtins_file,
+        .flags = c_flags,
+    });
+
+    switch (target.result.os.tag) {
+        .linux => {
+            lib_mod.linkSystemLibrary("m", .{});
+            if (!target.result.abi.isAndroid()) {
+                lib_mod.linkSystemLibrary("pthread", .{});
+            }
+        },
+        .macos => {
+            lib_mod.linkSystemLibrary("pthread", .{});
+            lib_mod.linkSystemLibrary("m", .{});
+        },
+        else => {},
+    }
+
+    const lib = b.addLibrary(.{
+        .name = "graphviz",
+        .linkage = .static,
+        .root_module = lib_mod,
+    });
+
+    if (cmake_needed) {
+        lib.step.dependOn(&cmake_cfg.step);
+    }
 
     const graphviz_mod = b.createModule(.{
         .root_source_file = b.path("src/bindings/graphviz.zig"),
@@ -176,18 +563,11 @@ fn buildGraphviz(
             .{ .name = "c", .module = c_mod },
         },
     });
-
-    graphviz_mod.addLibraryPath(b.path("vendor/graphviz/build/lib/cgraph"));
-    graphviz_mod.addLibraryPath(b.path("vendor/graphviz/build/lib/gvc"));
-    graphviz_mod.addLibraryPath(b.path("vendor/graphviz/build/lib/cdt"));
-
-    graphviz_mod.linkSystemLibrary("cgraph", .{});
-    graphviz_mod.linkSystemLibrary("gvc", .{});
-    graphviz_mod.linkSystemLibrary("cdt", .{});
+    graphviz_mod.linkLibrary(lib);
 
     return .{
         .module = graphviz_mod,
-        .step = &cmake_build.step,
+        .step = &lib.step,
     };
 }
 
